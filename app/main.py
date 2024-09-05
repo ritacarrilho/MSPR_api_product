fef0ffb054f37228868e9bdc757e4f04fc97e8c6
Rita Carrilho Lameira <RLameira@slb.com>
2024-09-05 13:51:07 +0200

    create api

---
+++ app/main.py
@@ -0,0 +1,36 @@
+"""
+Module with all the API endpoints for products management.
+It includes the operations of creation, lecture, update and delete of products>
+"""
+
+from typing import List
+from fastapi import FastAPI, HTTPException, Depends
+
+
+app = FastAPI()
+
+@app.get("/", response_model=dict, tags=["Health Check"])
+def api_status():
+    """
+    Verifies the API status.
+
+    Returns:
+        dict: dict with the API status.
+    """
+    return {"status": "running"}
+
+
+
+@app.get("/products/", tags=["Products"])
+def get_products():
+    return {"get products": "ok"}
+
+
+@app.get("/products/{id}", tags=["Products"])
+def get_products():
+    return {"get product by id": "ok"}
+
+
+@app.get("/products/{name}", tags=["Products"])
+def get_products():
+    return {"get product by name": "ok"}
