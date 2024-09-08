e32cc68fc6016546966bda17f672ed40966baec7
melissa billard <billardmelissa1@gmail.com>
2024-09-08 13:40:44 +0200

    correctif import data base

--- database/scripts/create_BDD.py
+++ database/scripts/create_BDD.py
@@ -14,7 +14,8 @@ config = {
 db_name = 'product_db'
 
 # Chemin du fichier JSON
-json_file_path = os.path.join('..', 'data', 'data.json')
+# json_file_path = os.path.join('..', 'data', 'data.json')
+json_file_path = os.path.join(os.getcwd(), 'database/data', 'data.json')
 
 # Connexion à MySQL
 try:
