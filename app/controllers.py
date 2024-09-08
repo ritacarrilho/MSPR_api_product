8eb7a3d23942533536e80b49c710d6d44fb1005b
melissa billard <billardmelissa1@gmail.com>
2024-09-08 18:24:31 +0200

    req endpoint post ok

--- app/controllers.py
+++ app/controllers.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session
 from .models import Product
 from fastapi import HTTPException
+from .schemas import StockCreate
+from .schemas import ProductCreate
 
 def get_all_products(db: Session):
     return db.query(Product).all()
@@ -19,3 +21,17 @@ def get_stock_by_id(db: Session, stock_id: int):
     if stock is None:
         raise HTTPException(status_code=404, detail="Stock not found")
     return stock
+
+def create_product(db: Session, product: ProductCreate):
+    db_product = Product(**product.dict())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+def create_stock(db: Session, stock: StockCreate):
+    db_stock = Stock(**stock.dict())
+    db.add(db_stock)
+    db.commit()
+    db.refresh(db_stock)
+    return db_stock
