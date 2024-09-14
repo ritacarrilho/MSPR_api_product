b59fb441c9f9b2acb10be6d03ec4b16ae8b971db
melissa billard <billardmelissa1@gmail.com>
2024-09-14 10:53:47 +0200

    update models BDD + insert

--- database/scripts/init.sql
+++ database/scripts/init.sql
@@ -1,46 +1,107 @@
-CREATE TABLE products(
-   id_product INT AUTO_INCREMENT,
-   created_at DATETIME NOT NULL,
-   name VARCHAR(50) NOT NULL,
-   price DECIMAL(10,2) NOT NULL,
-   description VARCHAR(150),
-   color VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id_product),
-   UNIQUE(name)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : sam. 14 sep. 2024 à 08:53
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.1.13
 
-CREATE TABLE stocks(
-   id_stocks INT AUTO_INCREMENT,
-   quantity INT NOT NULL,
-   id_product INT NOT NULL,
-   PRIMARY KEY(id_stocks),
-   FOREIGN KEY(id_product) REFERENCES products(id_product)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-INSERT INTO products (created_at, name, price, description, color)
-VALUES 
-('2023-08-30 13:25:32', 'Emily Mraz Jr.', 738.00, 'New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart', 'magenta'),
-('2023-08-30 07:29:29', 'Lonnie Schuppe DDS', 753.00, 'The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality', 'fuchsia'),
-('2023-08-29 14:39:54', 'Clint Boyer', 746.00, 'Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals', 'blue'),
-('2023-08-30 06:55:20', 'Kottea CK150S', 599.00, 'La Kottea CK150S est une machine expresso polyvalente, mais simple d\'utilisation grâce aux filtres pressurisés et filtre simples fournis.', 'olive'),
-('2023-08-30 06:44:19', 'Marianne VonRueden', 154.00, 'New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016', 'teal'),
-('2023-08-30 02:49:49', 'Dr. Jeanne Streich', 608.00, 'The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality', 'tan'),
-('2023-08-30 10:53:17', 'Ernestine Willms', 662.00, 'The Football Is Good For Training And Recreational Purposes', 'mint green'),
-('2023-08-30 02:05:10', 'Yvonne Fisher', 358.00, 'The Apollotech B340 is an affordable wireless mouse with reliable connectivity, 12 months battery life and modern design', 'mint green'),
-('2023-08-30 04:12:31', 'Lynn Skiles', 963.00, 'The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality', 'blue'),
-('2023-08-29 19:07:33', 'April McDermott', 14.00, 'The Apollotech B340 is an affordable wireless mouse with reliable connectivity, 12 months battery life and modern design', 'orchid');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Insert sample data into stocks table
-INSERT INTO stocks (quantity, id_product)
-VALUES 
-(56558, 1),
-(65152, 2),
-(90214, 3),
-(5549, 4),
-(25994, 5),
-(82498, 6),
-(92876, 7),
-(68486, 8),
-(5563, 9),
-(81671, 10);
+--
+-- Base de données : `product_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id_category` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(150) DEFAULT NULL,
+  `id_product` int NOT NULL,
+  PRIMARY KEY (`id_category`),
+  UNIQUE KEY `name` (`name`),
+  KEY `id_product` (`id_product`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `categories`
+--
+
+INSERT INTO `categories` (`id_category`, `name`, `description`, `id_product`) VALUES
+(1, 'Café Arabica', 'Café de haute qualité avec un goût doux et fruité.', 0),
+(2, 'Café Robusta', 'Café fort avec un goût amer, idéal pour les expresso.', 0),
+(3, 'Café Décaféiné', 'Café sans caféine pour une consommation sans stimulant.', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id_product` int NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `update_at` datetime DEFAULT NULL,
+  `name` varchar(50) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `description` varchar(150) DEFAULT NULL,
+  `origin` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_product`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `products`
+--
+
+INSERT INTO `products` (`id_product`, `created_at`, `update_at`, `name`, `price`, `description`, `origin`) VALUES
+(1, '0000-00-00 00:00:00', NULL, 'Café Arabica', '12.99', 'Café de haute qualité avec un goût doux et fruité.', 'Colombie'),
+(2, '0000-00-00 00:00:00', NULL, 'Café Robusta', '10.99', 'Café fort avec un goût amer, idéal pour les expresso.', 'Vietnam'),
+(3, '0000-00-00 00:00:00', NULL, 'Café Décaféiné', '11.99', 'Café sans caféine pour une consommation sans stimulant.', 'Brésil');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stocks`
+--
+
+DROP TABLE IF EXISTS `stocks`;
+CREATE TABLE IF NOT EXISTS `stocks` (
+  `id_stocks` int NOT NULL AUTO_INCREMENT,
+  `quantity` int NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `update_at` datetime DEFAULT NULL,
+  `id_product` int NOT NULL,
+  PRIMARY KEY (`id_stocks`),
+  KEY `id_product` (`id_product`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `stocks`
+--
+
+INSERT INTO `stocks` (`id_stocks`, `quantity`, `created_at`, `update_at`, `id_product`) VALUES
+(1, 100, NULL, NULL, 1),
+(2, 50, NULL, NULL, 2),
+(3, 75, NULL, NULL, 3);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
